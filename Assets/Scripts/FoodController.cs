565795183708f4fcf8947344069329402c0905fb
Parva <parvadesai404@gmail.com>
2024-10-18 17:25:25 +0530

    [Fix] refactoring & cleanup

--- Assets/Scripts/FoodController.cs
+++ Assets/Scripts/FoodController.cs
@@ -3,36 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FoodController : MonoBehaviour
+public class FoodController : ItemController<Food>
 {
-    [SerializeField] private Collider2D SpawningArea;
-    [SerializeField] private List<Food> FoodObjects;
-    [SerializeField] private float spawnRate;
     
-    private ItemSpawner<Food> itemSpawner;
-    
-    void Start()
-    {
-        itemSpawner = new ItemSpawner<Food>(SpawningArea, this.transform, FoodObjects, spawnRate);
-        
-        itemSpawner.StartSpawning();
-    }
-    
-    void Update()
-    {
-        if (GameManager.Instance.IsGameOver)
-            return;
-        
-        itemSpawner.UpdateSpawner();
-    }
-
-    public List<Food> GetItems()
-    {
-        return itemSpawner.GetActiveItems();
-    }
-
-    public void OnItemCollect(Food item)
-    {
-        itemSpawner.RecycleItem(item);
-    }
 }
--- Assets/Scripts/PowerUpController.cs
+++ Assets/Scripts/PowerUpController.cs
@@ -2,36 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PowerUpController : MonoBehaviour
+public class PowerUpController : ItemController<PowerUp>
 {
-    [SerializeField] private Collider2D SpawningArea;
-    [SerializeField] private List<PowerUp> PowerUpObjects;
-    [SerializeField] private float spawnRate;
     
-    private ItemSpawner<PowerUp> itemSpawner;
-    
-    void Start()
-    {
-        itemSpawner = new ItemSpawner<PowerUp>(SpawningArea, this.transform, PowerUpObjects, spawnRate);
-        
-        itemSpawner.StartSpawning();
-    }
-    
-    void Update()
-    {
-        if (GameManager.Instance.IsGameOver)
-            return;
-        
-        itemSpawner.UpdateSpawner();
-    }
-    
-    public List<PowerUp> GetItems()
-    {
-        return itemSpawner.GetActiveItems();
-    }
-    
-    public void OnItemCollect(PowerUp item)
-    {
-        itemSpawner.RecycleItem(item);
-    }
 }
